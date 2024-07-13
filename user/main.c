f3400cc3e9442e827517b51662f542db81dfa61b
bigbin20010203 <bigbin01@163.com>
2024-07-13 12:37:55 +0800

    fengmingqi

--- user/main.c
+++ user/main.c
@@ -3,19 +3,19 @@
 
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	GPIO_InitTypeDef  GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//开漏可以输出低电平
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-	GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	//GPIO_ResetBits(GPIOB,GPIO_Pin_0);
+	//GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_RESET);
 	while(1)
 	{
-		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+		GPIO_WriteBit(GPIOB,GPIO_Pin_12,Bit_RESET);//Bit_RESET = (bitaction)0
 		Delay_ms(500);
-		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
+		GPIO_WriteBit(GPIOB,GPIO_Pin_12,Bit_SET);
 		Delay_ms(500);
 	}
 }
